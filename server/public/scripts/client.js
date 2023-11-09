8cca5d83a6cee8b7bc4b095548921f7ce2979dbd
Jacob Larson <hockeybobber@comcast.net>
2023-11-09 17:11:17 -0600

    added get routes in both client and server side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,27 @@
 console.log('JS is sourced!');
+
+getTodos()
+
+//get route
+function getTodos() {
+    console.log('in get route client side');
+
+    // axios call to server to get todos
+  axios({
+    url: '/todos',
+    method: 'GET'
+  }).then((response) => {
+    //will put render function here
+  })
+}
+
+//render route
+
+
+//post route
+
+
+//put route
+
+
+//delete route
--- server/routes/todos.router.js.js
+++ server/routes/todos.router.js.js
@@ -2,6 +2,34 @@ const router = require('express').Router();
 const pool = require('../modules/pool');
 
 
+//get route
+router.get('/', (req, res) => {
+    console.log('in get route');
+
+    let queryText = `
+    SELECT * FROM "todos" 
+    ORDER BY "id";
+    `
+  pool.query(queryText)
+    .then(dbResult => {
+        console.log('dbResult', dbResult.rows);
+        // Sends back the results in an object
+        res.send(dbResult.rows);
+  })
+  .catch(error => {
+    console.log('error getting todos', error);
+    res.sendStatus(500);
+  });
+})
+
+//post route
+
+
+//put route
+
+
+//delete route
+
 
 
 module.exports = router;
